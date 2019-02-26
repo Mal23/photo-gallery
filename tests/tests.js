129bf06427e540c4b88deb62be1a6786f8f1721d
Mallarie <mallariemilano@gmail.com>
2019-02-25 17:46:46 -0800

    created initial files and folders, hard coded images into html, and began test

---
+++ tests/tests.js
@@ -0,0 +1,16 @@
+import './html-equal.js';
+
+test('photo gallery test', function(assert) {
+    const images = {
+        'url': 'http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg',
+        'title': 'UniWhal',
+        'description': 'A unicorn and a narwhal nuzzling their horns',
+        'keyword': 'narwhal',
+        'horns': 1
+    }; 
+
+    const result = {
+
+    };
+    assert.htmlEqual(images, result);
+});
