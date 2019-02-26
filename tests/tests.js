d118980ca6ccedd9a11e117262dbe4621f70344e
Mallarie <mallariemilano@gmail.com>
2019-02-25 21:52:27 -0800

    created failing test without function

--- tests/tests.js
+++ tests/tests.js
@@ -1,5 +1,7 @@
 import './html-equal.js';
 
+const test = QUnit.test;
+
 test('photo gallery test', function(assert) {
     const images = {
         'url': 'http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg',
@@ -9,8 +11,12 @@ test('photo gallery test', function(assert) {
         'horns': 1
     }; 
 
-    const result = {
+    const result = gallery(images);
+    const expected = 
+    `<li>
+    <h2>UniWhal</h2>
+    <img src="http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg" alt="A unicorn and a narwhal nuzzling their horns">
+</li>`;
 
-    };
-    assert.htmlEqual(images, result);
+    assert.htmlEqual(result, expected);
 });
